f89abfb62ee135e0fd107720ac45a311c977b2e0
camilaViquez <32073514+camilaViquez@users.noreply.github.com>
2018-02-22 09:15:33 -0600

    Register view and register in db

--- app/src/main/java/cr/ac/itcr/hellowworld2018/MainActivity.java
+++ app/src/main/java/cr/ac/itcr/hellowworld2018/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+//HOLaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
